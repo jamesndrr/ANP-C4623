bd5e99ecf3cb9d208b5ae56f7b6e972b531afdb3
jamesndrr <126569928+jamesndrr@users.noreply.github.com>
2023-03-01 18:06:31 +0530

    Add files via upload

---
+++ Practicals/Student.java
@@ -0,0 +1,29 @@
+package corejava;
+
+class Student {
+	private int Id;
+	private String Name;
+	private int Age;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+	
+	
+	
+}
