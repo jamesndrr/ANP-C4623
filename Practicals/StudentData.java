e337becc19ab122f90d80e0884822cfb8cb61527
jamesndrr <126569928+jamesndrr@users.noreply.github.com>
2023-03-01 17:29:53 +0530

    Add files via upload

---
+++ Practicals/StudentData.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class StudentData 
+{
+
+	public static void main(String[] args) 
+	{
+		Student a = new Student();
+		a.setName("James Nadar");
+		a.setId(20);
+		a.setAge(20);
+		
+		System.out.println("Student ID: "+a.getId()+"\n"+"Student Name: "+a.getName()+"\n"+"Student Age: "+a.getAge()+"\n");
+
+	}
+
+}
