166d26ad8eb7e32397ef1fa62750572c9eb04416
jamesndrr <126569928+jamesndrr@users.noreply.github.com>
2023-03-02 18:30:36 +0530

    Update AbstractionTest.java

--- Practicals/AbstractionTest.java
+++ Practicals/AbstractionTest.java
@@ -3,7 +3,7 @@ package corejava;
 public class AbstractionTest {
 
 	public static void main(String[] args) {
-		AbstractionExample a = AbstractionExample(20,"James","Ghatkopar",20000);
+		AbstractionData a = new AbstractionData(20,"James","Ghatkopar",20000);
 		a.mailCheck();		
 	}
 
