797eccca74eaf7d5a8b1a9b0df8e0d396aad1afe
jamesndrr <126569928+jamesndrr@users.noreply.github.com>
2023-04-07 17:23:18 +0530

    Add files via upload

---
+++ Practicals/LinkedListEx.java
@@ -0,0 +1,25 @@
+package collection;
+
+import java.util.*;
+
+public class LinkedListEx {
+
+	public static void main(String[] args) {
+		LinkedList<String> lnd=new LinkedList<String>();
+		
+		//Adding object in ArrayList
+		lnd.add("car");
+		lnd.add("bike");
+		lnd.add("cycle");
+		lnd.add("bullock-cart");
+		
+		//traversing list through iterator
+		Iterator itr=lnd.iterator();
+		
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+
+}
